81d5fee2aee048c0af8a02b55e9bc266d4b85bfc
daniel_onyenwee <danielonyenwee@gmail.com>
2022-11-11 03:13:27 +0100

    ➕ add component "alert-box", "default-text-input", "sigin-tab" and "signup-tab"

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import AlertBox from "./alert-box.svelte"
+import DefaultTextInput from "./default-text-input.svelte"
+import SigninTab from "./signin-tab.svelte"
+import SignupTab from "./signup-tab.svelte"
+
+export {
+    AlertBox,
+    SigninTab,
+    SignupTab,
+    DefaultTextInput
+}
